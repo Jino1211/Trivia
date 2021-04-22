23e3cd13c490eec645c7a2dad76569915c136aff
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-04-22 09:28:10 +0300

    finish query for type1 questions

--- db_manager/query.js
+++ db_manager/query.js
@@ -2,9 +2,32 @@ const { Op, Sequelize } = require("sequelize");
 const { questions } = require("./models");
 const models = require(`./models`);
 
-// questions.findOne({ order: Sequelize.literal("rand()") }).then((result) => {
+//checking the kind of the type one question (max/min).
+const checkIncludesText = (question) => {
+  const maxWords = ["highest", "most", "largest"];
+  return maxWords.some((word) => question.includes(word));
+};
+
+//function for handling the type 1 questions.
+const type1 = (countries, { question, column }) => {
+  const sendData = { question: "", option: [] };
+  const isMax = checkIncludesText(question);
+  sendData.question = question;
+  countries.map((country) => sendData.option.push(country.Country));
+  countries.sort((a, b) => b[column] - a[column]);
+  isMax
+    ? (savedAnswer = countries[0].toJSON())
+    : (savedAnswer = countries[3].toJSON());
+  console.log(sendData);
+  console.log(isMax);
+  console.log(savedAnswer);
+};
+
+const type2 = () => {};
+
+// main query for navigating between questions types.
 questions
-  .findOne({ order: [["id", "desc"]], where: { type: 1 } })
+  .findOne({ order: Sequelize.literal("rand()"), where: { type: 2 } })
   .then((result) => {
     const table = models[`${result["table"]}`];
     console.log(
@@ -20,17 +43,10 @@ questions
         table
           .findAll({ order: Sequelize.literal("rand()"), limit: 4 })
           .then((countries) => {
-            const options = [];
-            countries.map((country) => options.push(country.Country));
-            countries.sort((a, b) => a.Literacy_percent - b.Literacy_percent);
-            console.log(countries);
-            console.log(options);
+            type1(countries, result);
           });
       case 2:
 
       case 3:
     }
-    table
-      .findOne({ order: Sequelize.literal("rand()") })
-      .then((res) => console.log(res.toJSON()));
   });
