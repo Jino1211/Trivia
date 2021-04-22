8cabc88429871aaf22612df63b7abd40219913d9
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-04-22 15:14:24 +0300

    added save question query

--- db_manager/query.js
+++ db_manager/query.js
@@ -1,6 +1,7 @@
 const { Op, Sequelize } = require("sequelize");
-const { questions } = require("./models");
+const { questions, saved_questions } = require("./models");
 const models = require(`./models`);
+let currentQuestion = {};
 
 //checking the kind of the type 1,3 questions (max/min).
 const checkIncludesText = (question) => {
@@ -10,87 +11,132 @@ const checkIncludesText = (question) => {
 
 //function for handling the type 1 questions.
 const type1 = (countries, { question, column }) => {
-  const sendData = { question: "", options: [], answer: "" };
+  currentQuestion = { question: "", options: [], answer: "" };
   const isMax = checkIncludesText(question);
-  sendData.question = question;
-  countries.map((country) => sendData.options.push(country.Country));
+  const noDashColumn = column.replace(/\_/g, " ");
+  currentQuestion.question = question;
+  countries.map((country) => currentQuestion.options.push(country.Country));
   countries.sort((a, b) => b[column] - a[column]);
   isMax
     ? (answer =
         "In " +
         countries[0].Country +
         " the " +
-        column +
+        noDashColumn +
         " is: " +
         countries[0][column])
     : (answer =
         "In " +
         countries[3].Country +
         " the " +
-        column +
+        noDashColumn +
         " is: " +
         countries[3][column]);
-  sendData.answer = answer;
+  currentQuestion.answer = answer;
 
-  console.log(sendData);
+  console.log(currentQuestion);
 };
 
 //function for handling the type 2 questions.
 const type2 = (countries, { question, column }) => {
-  const sendData = { question: "", options: [], answer: "" };
+  currentQuestion = { question: "", options: [], answer: "" };
   const ranAns = Math.floor(Math.random() * 4);
-  sendData.question = question + " " + countries[ranAns].Country;
+  currentQuestion.question = question + " " + countries[ranAns].Country;
   const answer = countries[ranAns][column];
-  countries.map((country) => sendData.options.push(country[column]));
-  sendData.answer = answer;
+  countries.map((country) => currentQuestion.options.push(country[column]));
+  currentQuestion.answer = answer;
 
-  console.log(sendData);
+  console.log(currentQuestion);
 };
 
 //function for handling the type 3 questions.
 const type3 = (countries, { question, column }) => {
-  const sendData = { question: "", options: [true, false], answer: "" };
-  sendData.question =
+  currentQuestion = { question: "", options: [true, false], answer: "" };
+  currentQuestion.question =
     question
       .replace("variable-x", countries[0].Country)
       .replace("variable-y", countries[1].Country) + "?";
   const answer = countries[0][column] > countries[1][column] ? true : false;
-  sendData.answer = answer;
-  console.log(sendData);
+  currentQuestion.answer = answer;
+
+  console.log(currentQuestion);
 };
 
 // main query for navigating between questions types.
-questions.findOne({ order: Sequelize.literal("rand()") }).then((result) => {
-  const table = models[`${result["table"]}`];
+questions
+  .findOne({ order: Sequelize.literal("rand()"), where: { type: 1 } })
+  .then((result) => {
+    const table = models[`${result["table"]}`];
 
-  switch (result.type) {
-    case 1:
-      table
-        .findAll({ order: Sequelize.literal("rand()"), limit: 4 })
-        .then((countries) => {
-          type1(countries, result);
-        });
-      break;
-    case 2:
-      table
-        .findAll({
-          order: Sequelize.literal("rand()"),
-          group: result.column,
-          limit: 4,
-        })
-        .then((countries) => {
-          type2(countries, result);
-        });
-      break;
+    switch (result.type) {
+      case 1:
+        table
+          .findAll({ order: Sequelize.literal("rand()"), limit: 4 })
+          .then((countries) => {
+            type1(countries, result);
+          });
+        break;
+      case 2:
+        table
+          .findAll({
+            order: Sequelize.literal("rand()"),
+            group: result.column,
+            limit: 4,
+          })
+          .then((countries) => {
+            type2(countries, result);
+          });
+        break;
 
-    case 3:
-      table
-        .findAll({
-          order: Sequelize.literal("rand()"),
-          limit: 2,
-        })
-        .then((countries) => {
-          type3(countries, result);
-        });
-  }
-});
+      case 3:
+        table
+          .findAll({
+            order: Sequelize.literal("rand()"),
+            limit: 2,
+          })
+          .then((countries) => {
+            type3(countries, result);
+          });
+    }
+  });
+
+const saveQuestion = (rate, currentQuestion) => {
+  saved_questions
+    .create({
+      question: currentQuestion.question,
+      option_1: currentQuestion.options[0],
+      option_2: currentQuestion.options[1],
+      option_3: currentQuestion.options[2],
+      option_4: currentQuestion.options[3],
+      answer: currentQuestion.answer,
+      rate: rate,
+      amount_of_times_rated: 1,
+      created_at: new Date().toLocaleDateString(),
+      updated_at: new Date().toLocaleDateString(),
+    })
+    .then((x) => console.log(x.toJSON()));
+};
+
+const updateSaveQuestion = (savedQuestion, { question }, rate) => {
+  saved_questions
+    .update(
+      {
+        rate:
+          (savedQuestion.rate + rate) /
+          (savedQuestion.amount_of_times_rated + 1),
+        amount_of_times_rated: savedQuestion.amount_of_times_rated + 1,
+      },
+      { where: { question: question } }
+    )
+    .then((x) => console.log(x));
+};
+
+const handleNewRate = async () => {
+  const { rate } = req.body;
+  const result = await saved_questions.findOne({
+    where: { question: currentQuestion.question },
+  });
+  result
+    ? updateSaveQuestion(result, currentQuestion, rate)
+    : saveQuestion(rate, currentQuestion);
+};
