5c3133574c0464ee5b05c2794cc8e86a61a52598
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-04-24 19:55:31 +0300

    adding react for client side

---
+++ client/src/components/Game.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import  from "react";
+
+export default function Game() {
+  return (
+    <div>
+      <Timer />
+      <Question />
+      <div>
+        <Option />
+        <Option />
+        <Option />
+        <Option />
+        <Rate />
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to trivia!</h1>
+      <button>Start New Game!</button>
+      <button>Go to Winners board</button>
+    </div>
+  );
+}
