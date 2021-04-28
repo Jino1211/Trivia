bbec8d43a254369e29187e0a864db074c21a436e
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-04-28 19:26:25 +0300

    moving files to server folder

---
+++ db_manager/server/users.js
@@ -0,0 +1,120 @@
+require("dotenv").config();
+const { Router } = require("express");
+const { hashSync, compare } = require("bcrypt");
+const { sign, verify } = require("jsonwebtoken");
+const {
+  saveNewRegister,
+  checkUserExist,
+  generateWeightedSavedQuestionArr,
+} = require("./utils");
+const { historyOfPlayer } = require("./api");
+const users = Router();
+
+//Middleware for checking if the token gotten from the req is valid
+const validToken = async (req, res, next) => {
+  const accessToken = await req.cookie["accessToken"];
+  if (!accessToken) {
+    return res.status(401).json({ message: "Access token is required" });
+  }
+  verify(accessToken, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid access token" });
+    }
+    req.user = decoded;
+    next();
+  });
+  console.log(accessToken, refreshToken);
+};
+
+//Entry point for create new user
+users.post("/register", async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    const isExist = await checkUserExist(email);
+
+    if (isExist)
+      return res.status(409).json({ message: "User already exists" });
+
+    const hashPassword = hashSync(password, 10);
+    await saveNewRegister(email, userName, hashPassword);
+
+    res.status(201).json({ message: "User was successfully created" });
+  } catch (err) {
+    res.status(400).json({ message: "We have a problem with our server" });
+  }
+});
+
+///////////////////////////////////////
+// users.post("/createuser", (req, res) => {
+//   const { user } = req.body;
+
+//   const { difficulty } = req.body;
+//   historyOfPlayer.user = user;
+//   historyOfPlayer.difficulty = difficulty;
+//   historyOfPlayer.score = 0;
+//   historyOfPlayer.playerQuestionsAndRates = [];
+//   generateWeightedSavedQuestionArr()
+//     .then(() => {
+//       res.status(200).json({ message: "User was successfully created" });
+//     })
+//     .catch(() =>
+//       res.status(200).json({ massage: "Cannot pull saved questions" })
+//     );
+// });
+
+//Entry point to verify users when there try to login
+users.post("/login", async (req, res) => {
+  const { difficulty } = req.body;
+  const { email, password } = req.body;
+  generateWeightedSavedQuestionArr();
+  console.log("after generate");
+  try {
+    const user = await checkUserExist(email);
+    console.log(user);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const isPasswordCorrect = await compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      console.log("if !password");
+      return res
+        .status(403)
+        .json({ message: "Email or password is incorrect" });
+    }
+    historyOfPlayer.user = user.user_name;
+    historyOfPlayer.difficulty = difficulty;
+    historyOfPlayer.score = 0;
+    historyOfPlayer.playerQuestionsAndRates = [];
+    const dataOfUser = { email: user.email, userName: user.userName };
+
+    const accessToken = sign(dataOfUser, process.env.SECRET_KEY, {
+      expiresIn: "1m",
+    });
+    const refreshToken = sign(dataOfUser, process.env.REFRESH_KEY, {
+      expiresIn: "1d",
+    });
+
+    res
+      .status(200)
+      .cookie("accessToken", accessToken)
+      .cookie("refreshToken", refreshToken)
+      .json({ name: user.user_name });
+  } catch (err) {
+    res.status(400).json({ message: "We problem with our server" });
+  }
+});
+
+//Entry point for clearing cookies(refresh token & access token)
+users.post("/logout", validToken, (req, res) => {
+  res
+    .clearCookie(accessToken)
+    .clearCookie(refreshToken)
+    .status(200)
+    .json({ message: "token cleared" });
+});
+
+module.exports = { users, validToken };
---
+++ db_manager/server/utils.js
@@ -0,0 +1,296 @@
+const { Op, Sequelize, where } = require("sequelize");
+const {
+  questions,
+  saved_questions,
+  users,
+  registeredUsers,
+} = require("../models");
+const models = require(`../models`);
+let question;
+const history = [];
+const questionsDuplicateByWeight = [];
+
+//checking the kind of the type 1,3 questions (max/min).
+const checkIncludesText = (question) => {
+  const maxWords = ["highest", "higher", "most", "largest", "more", "larger"];
+  return maxWords.some((word) => question.includes(word));
+};
+
+//function for handling the type 1 questions.
+const type1 = (countries, { question, column }) => {
+  const currentQuestion = { question: "", options: [], answer: "" };
+  const isMax = checkIncludesText(question);
+  const noDashColumn = column.replace(/\_/g, " ");
+  currentQuestion.question = question;
+  countries.map((country) => currentQuestion.options.push(country.Country));
+  countries.sort((a, b) => b[column] - a[column]);
+  // isMax ? (answer = countries[0].Country) : (answer = countries[3].Country);
+  answer = isMax ? countries[0].Country : countries[3].Country;
+  currentQuestion.answer = answer;
+
+  return currentQuestion;
+};
+
+//function for handling the type 2 questions.
+const type2 = (countries, { question, column }) => {
+  const currentQuestion = { question: "", options: [], answer: "" };
+  const ranAns = Math.floor(Math.random() * 4);
+  currentQuestion.question = question + " " + countries[ranAns].Country;
+  const answer = countries[ranAns][column];
+  countries.map((country) => currentQuestion.options.push(country[column]));
+  currentQuestion.answer = answer;
+
+  return currentQuestion;
+};
+
+//function for handling the type 3 questions.
+const type3 = (countries, { question, column }) => {
+  const currentQuestion = {
+    question: "",
+    options: ["true", "false"],
+    answer: "",
+  };
+  currentQuestion.question =
+    question
+      .replace("variable-x", countries[0].Country)
+      .replace("variable-y", countries[1].Country) + "?";
+  const answer = countries[0][column] > countries[1][column] ? "true" : "false";
+  currentQuestion.answer = answer;
+
+  return currentQuestion;
+};
+
+//function for handling the saved questions.
+const typeSaved = (saved) => {
+  const currentQuestion = { question: "", options: [], answer: "" };
+
+  currentQuestion.question = saved.question;
+  currentQuestion.options = saved.option_3
+    ? [saved.option_1, saved.option_2, saved.option_3, saved.option_4]
+    : saved.option_1 === "0"
+    ? ["false", "true"]
+    : ["true", "false"];
+  currentQuestion.answer = saved.answer;
+  return currentQuestion;
+};
+
+//check if the current question was already displayed to the client
+const isInHistory = () => {
+  let bool = false;
+  history.forEach((ques) => {
+    if (ques.question === question.question && ques.answer === question.answer)
+      bool = true;
+  });
+  return bool;
+};
+
+//Function for handling easy questions by their type.
+const handleEasyTypes = async (result, table) => {
+  let countries;
+  switch (result.type) {
+    case 1:
+      countries = await table.findAll({
+        order: Sequelize.literal("rand()"),
+        limit: 4,
+      });
+      question = type1(countries, result);
+      break;
+
+    case 2:
+      countries = await table.findAll({
+        order: Sequelize.literal("rand()"),
+        group: result.column,
+        limit: 4,
+      });
+      question = type2(countries, result);
+      break;
+
+    case 3:
+      countries = await table.findAll({
+        order: Sequelize.literal("rand()"),
+        limit: 2,
+      });
+      question = type3(countries, result);
+      break;
+  }
+};
+
+//Function for handling hard questions by their type
+const handleDifficultTypes = async (result, table) => {
+  let countries;
+  const length = (await table.count({})) - 4;
+  const randomRow = Math.floor(Math.random() * length);
+  const shuffle = (array) =>
+    array.sort(() => {
+      return 0.5 - Math.random();
+    });
+  switch (result.type) {
+    case 1:
+      countries = await table.findAll({
+        order: [[result.column]],
+        limit: [randomRow, 4],
+      });
+      countries = shuffle(countries);
+      question = type1(countries, result);
+      break;
+
+    case 2:
+      countries = await table.findAll({
+        order: [[result.column]],
+        group: result.column,
+        limit: [randomRow, 4],
+      });
+      countries = shuffle(countries);
+      question = type2(countries, result);
+      break;
+
+    case 3:
+      countries = await table.findAll({
+        order: [[result.column]],
+        limit: [randomRow, 2],
+      });
+      countries = shuffle(countries);
+      question = type3(countries, result);
+      break;
+  }
+};
+
+// main query for navigating between questions types.
+const getQuestion = async (third, difficulty) => {
+  if (third && questionsDuplicateByWeight.length !== 0) {
+    question = typeSaved(getFromWeightedArr());
+  } else {
+    const result = await questions.findOne({
+      order: Sequelize.literal("rand()"),
+    });
+    const table = models[`${result["table"]}`];
+
+    difficulty === "hard"
+      ? await handleDifficultTypes(result, table)
+      : await handleEasyTypes(result, table);
+  }
+  if (isInHistory()) getQuestion();
+  history.push(question);
+  return question;
+};
+
+//Function for saving a new rated question.
+const saveNewQuestion = (usersQuestion, difficulty, score) => {
+  saved_questions
+    .create({
+      question: usersQuestion.question,
+      option_1: usersQuestion.options[0],
+      option_2: usersQuestion.options[1],
+      option_3: usersQuestion.options[2],
+      option_4: usersQuestion.options[3],
+      answer: usersQuestion.answer,
+      rates_multiply_points: score * usersQuestion.rate,
+      points_per_player: score,
+      rate: usersQuestion.rate,
+      amount_of_times_rated: 1,
+      difficulty: difficulty,
+      created_at: new Date().toLocaleDateString(),
+      updated_at: new Date().toLocaleDateString(),
+    })
+    .then((x) => console.log(x.toJSON()));
+  console.log("finish save New Question ");
+};
+
+//Function for updating an already rated question.
+const updateSavedQuestion = (result, usersQuestion, score) => {
+  saved_questions
+    .update(
+      {
+        rates_multiply_points:
+          result.rates_multiply_points + score * usersQuestion.rate,
+        points_per_player: result.points_per_player + score,
+        rate:
+          (result.rates_multiply_points + score * usersQuestion.rate) /
+          (result.points_per_player + score),
+        amount_of_times_rated: result.amount_of_times_rated + 1,
+      },
+      { where: { question: usersQuestion.question } }
+    )
+    .then((x) => console.log(x));
+};
+
+//Function to distinguish between already rated questions to new rated questions.
+const handleNewRate = async (usersQuestion, difficulty, score) => {
+  const result = await saved_questions.findOne({
+    where: { question: usersQuestion.question },
+  });
+  result
+    ? updateSavedQuestion(result, usersQuestion, score)
+    : saveNewQuestion(usersQuestion, difficulty, score);
+};
+
+//Function to save user and there score in the database.
+const saveUser = ({ user, score }) => {
+  users
+    .create({
+      name: user,
+      score: score,
+    })
+    .then((x) => console.log(x));
+};
+
+//Function for generating an array of saved questions,with frequency based on their rate.
+const generateWeightedSavedQuestionArr = async () => {
+  const savedQuestions = await saved_questions.findAll({});
+  let totalRate = 0;
+  savedQuestions.map((question) => (totalRate += question.rate));
+  savedQuestions.map(
+    (question) =>
+      (question.weight = Math.floor((question.rate / totalRate) * 100))
+  );
+  savedQuestions.forEach((question) => {
+    for (let i = 0; i < question.weight; i++) {
+      questionsDuplicateByWeight.push(question);
+    }
+  });
+};
+
+//Function for getting a random question from the weighted array.
+const getFromWeightedArr = () => {
+  const randomQuestion = Math.floor(
+    Math.random() * questionsDuplicateByWeight.length
+  );
+
+  console.log(questionsDuplicateByWeight[randomQuestion].toJSON());
+  return questionsDuplicateByWeight[randomQuestion];
+};
+
+const getScoreBoard = async () => {
+  const scoreBoard = await users.findAll({
+    attributes: ["name", "score", "created_at"],
+    order: [["score", "DESC"]],
+  });
+
+  return scoreBoard;
+};
+
+const checkUserExist = async (email) => {
+  const isExist = await registeredUsers.findOne({ where: { email: email } });
+  return isExist;
+};
+
+const saveNewRegister = async (email, userName, password) => {
+  const x = await registeredUsers.create({
+    email,
+    user_name: userName,
+    password,
+    created_at: new Date(),
+    updated_at: new Date(),
+  });
+  console.log(x.toJSON());
+};
+
+module.exports = {
+  getQuestion,
+  handleNewRate,
+  saveUser,
+  generateWeightedSavedQuestionArr,
+  getScoreBoard,
+  checkUserExist,
+  saveNewRegister,
+};
